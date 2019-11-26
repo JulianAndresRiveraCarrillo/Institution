40e745a3e08b72239267057259dd0076448343ab
Julianandresriveracarrillo <julianandresrivera29@hotmail.com>
2019-11-25 23:49:01 -0500

    Principal windows created

--- src/userinterface/Main.java
+++ src/userinterface/Main.java
@@ -1,10 +1,25 @@
 package userinterface;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application{
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		launch(args);
+	}
 
+	@Override
+	public void start(Stage arg0) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("Principal.fxml"));
+		Scene scene = new Scene(root);
+		arg0.setTitle("Institution");
+		arg0.setScene(scene);
+		arg0.setMaximized(true);
+		arg0.show();
 	}
 
 }
