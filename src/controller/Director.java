631a74fa08843174f74b44e5499cb20187b314e5
Julianandresriveracarrillo <julianandresrivera29@hotmail.com>
2019-11-28 02:29:52 -0500

    user added on the login.fxml

--- src/controller/Director.java
+++ src/controller/Director.java
@@ -1,5 +1,55 @@
 package controller;
 
-public class Director {
+import java.io.IOException;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import model.*;
+public class Director {
+	
+	@FXML
+	Button profesor;
+	
+	@FXML
+	Button directorGrupo;
+	
+	@FXML
+	Button estudiante;
+	
+	@FXML
+	Button personero;
+	
+	private Login log;
+	
+	private Student st;
+	
+	private Profesor p;
+
+	@FXML
+	public void openTeacher(MouseEvent event) {
+		
+		
+		FXMLLoader fxml = new FXMLLoader(getClass().getResource("Profesor.fxml"));
+		try {
+			Parent root = fxml.load();
+			Stage stage = new Stage();
+			Scene scene = new Scene(root);
+			p = fxml.getController();
+			//p.setScene(scene);
+			stage.setTitle("PROFESOR");
+			stage.setScene(scene);
+			stage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+	
 }
