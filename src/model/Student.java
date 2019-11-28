e9a5fb7c36ee463668f8c248d7927324675ee163
Alejandra Diaz <alejandradp19@gmail.com>
2019-11-28 08:03:22 -0500

    Added javadoc in Student and Subject

--- src/model/Student.java
+++ src/model/Student.java
@@ -4,6 +4,11 @@ import java.io.Serializable;
 
 import exception.NotFoundException;
 
+/** 
+ *@author: Julian Rivera
+ *@author: Alejandra Diaz
+ *Class Student
+ */
 public class Student implements SubjectInterface,Serializable{
 	
 	/**
@@ -22,7 +27,16 @@ public class Student implements SubjectInterface,Serializable{
 	//relaciones 
 	private Subject studentSubjects;
 	
-	//metodo constructor
+	/**
+	 * Constructor that instantiates a Student
+	 * @param First name of the student
+	 * @param lastName Last name of the student
+	 * @param id Identification number of the student
+	 * @param age Age of the student
+	 * @param stratum Economic stratum of the student
+	 * @param phone Phone number of the student
+	 * @param address Direction where the student lives
+	 */
 	public Student(String firstName, String lastName, int id, int age, int stratum, String phone, String address) {
 		this.firstName = firstName;
 		this.lastName = lastName;
@@ -34,70 +48,130 @@ public class Student implements SubjectInterface,Serializable{
 	}
 
 	//metodos getters y setters
+	/**
+	 * returns the name of the student
+	 * @return first name of the student
+	 */
 	public String getFirstName() {
 		return firstName;
 	}
 
+	/**
+	 * Changes the first name of the student for the one in the parameters
+	 * @param firstName new name
+	 */
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
 
+	/**
+	 * returns the last name of the student
+	 * @return last name of the student
+	 */
 	public String getLastName() {
 		return lastName;
 	}
 
+	/**
+	 * Changes the last name of the student for the one in the parameters
+	 * @param lastName new last name
+	 */
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
 
+	/**
+	 * returns the id of the student
+	 * @return identification number of the student
+	 */
 	public int getId() {
 		return id;
 	}
 
+	/**
+	 * Changes id of the student for the one in the parameters
+	 * @param id new identification number
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
 
+	/**
+	 * returns the age of the student
+	 * @return age of the student
+	 */
 	public int getAge() {
 		return age;
 	}
 
+	/**
+	 * Changes the age of the student for the one in the parameters
+	 * @param age new age
+	 */
 	public void setAge(int age) {
 		this.age = age;
 	}
 
+	/**
+	 * returns the stratum of the student
+	 * @return economic stratum of the student
+	 */
 	public int getStratum() {
 		return stratum;
 	}
 
+	/**
+	 * Changes the stratum of the student for the one in the parameters
+	 * @param stratum new economic stratum
+	 */
 	public void setStratum(int stratum) {
 		this.stratum = stratum;
 	}
 
+	/**
+	 * returns the phone of the student
+	 * @return phone number of the student
+	 */
 	public String getPhone() {
 		return phone;
 	}
 
+	/**
+	 * Changes the phone of the student for the one in the parameters
+	 * @param phone new phone number
+	 */
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
 
+	/**
+	 * returns the address of the student
+	 * @return address of the student
+	 */
 	public String getAddress() {
 		return address;
 	}
 
+	/**
+	 * Changes the direction of the student for the one in the parameters
+	 * @param address new address
+	 */
 	public void setAddress(String address) {
 		this.address = address;
 	}
 
+	/**
+	 * returns the list of subjects that the student is studying
+	 * @return list of subjects
+	 */
 	public Subject getStudentSubjects() {
 		return studentSubjects;
 	}
 
-	public void setStudentSubjects(Subject studentSubjects) {
-		this.studentSubjects = studentSubjects;
-	}
-	
+	/**
+	 * Calculates the average grade of the semester
+	 * @return average grade
+	 */
 	public double semesterAverage() {
 		int c = 0;
 		int grades = 0;
@@ -108,11 +182,20 @@ public class Student implements SubjectInterface,Serializable{
 			grades += aux.average();
 			c++;
 			aux = aux.getNextSubject();
-		}a = grades/c;
+		}try{
+			a = grades/c;
+		}catch(ArithmeticException ae) {
+			ae.printStackTrace();
+		}
 		}
 		return a;
 	}
 	
+	/**
+	 * Searches a subject in the list of subjects of the student
+	 * @param code code of the subject searched
+	 * @return subject searched
+	 */
 	public Subject searchSubject(int code) {
 		return SubjectInterface.searchSubject(studentSubjects, code);
 	}
--- src/model/Subject.java
+++ src/model/Subject.java
@@ -6,7 +6,9 @@ import java.util.ArrayList;
 public abstract class Subject implements Serializable{
 	
 	/**
-	 * 
+	 * @author Julian Rivera
+	 * @author Alejandra Diaz
+	 * Class Subject
 	 */
 	private static final long serialVersionUID = 1L;
 	private ArrayList<Double> grades;
@@ -17,6 +19,12 @@ public abstract class Subject implements Serializable{
 	private Subject nextSubject;
 
 	//metodo constructor
+	/**
+	 * Constructor that instantiates a Subject
+	 * @param n name of the subject
+	 * @param id identification number of the subject
+	 * @param nextSubject attribute that identifies this data structure as a list of subjects
+	 */
 	public Subject(String n, int id,Subject nextSubject) {
 		name = n;
 		this.id = id;
@@ -25,45 +33,86 @@ public abstract class Subject implements Serializable{
 	}
 
 	//metodos getters y setters
+	/**
+	 * returns the next subject in the list of subjects
+	 * @return the next subject
+	 */
 	public Subject getNextSubject() {
 		return nextSubject;
 	}
 
+	/**
+	 * Changes the following subject in the list of subjects
+	 * @param id new subject
+	 */
 	public void setNextSubject(Subject nextSubject) {
 		this.nextSubject = nextSubject;
 	}
 
+	/**
+	 * returns the list (arraylist) of grades that a student has in this subject
+	 * @return arraylist of grades
+	 */
 	public ArrayList<Double> getGrade() {
 		return grades;
 	}
 
+	/**
+	 * returns the name of the subject
+	 * @return name of the subject
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Changes name of the student for the one in the parameters
+	 * @param name new name
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * returns the id of the subject
+	 * @return identification number of the subject
+	 */
 	public int getId() {
 		return id;
 	}
 
+	/**
+	 * Changes id of the subject for the one in the parameters
+	 * @param id new identification number
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
 	
+	/**
+	 * Calculates the average grade of a student in this subject
+	 * @return average grade
+	 */
 	public double average() {
 		int counter = 0;
 		int g = 0;
-		for(int i = 0; i<grades.size();i++) {
+		try{for(int i = 0; i<grades.size();i++) {
 			g+=grades.get(i);
 			counter++;
+		}
+		if(counter==0 || (g==0&&counter==0)) {
+			throw new ArithmeticException();
+		}}catch(ArithmeticException a) {
+			a.printStackTrace();
 		}
 		double av = g/counter;
 		return av;
 	}
 	
+	/**
+	 * Erases a grade from the arraylist of grades
+	 * @param grad grade that's going to be erased
+	 */
 	public void eraseGrade(double grad) {
 		boolean done = false;
 		for(int i = 0; i<grades.size() || !done;i++) {
@@ -74,6 +123,11 @@ public abstract class Subject implements Serializable{
 		}
 	}
 	
+	/**
+	 * Changes a grade of the arraylist of grades
+	 * @param p position of the grade to be changed
+	 * @param ng new grade
+	 */
 	public void modifyGrade(int p, double ng) {
 		grades.set(p, ng);
 	}
